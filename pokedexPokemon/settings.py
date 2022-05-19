4908f8d55978b68019d3365c387474a066a9d0b8
Juanma Díaz <juanma99dr@gmail.com>
2022-05-19 21:11:47 +0200

    runtime added

--- pokedexPokemon/settings.py
+++ pokedexPokemon/settings.py
@@ -129,8 +129,7 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
