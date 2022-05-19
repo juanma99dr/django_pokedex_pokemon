f702a98fb997d268233a97a7d724665d180d6497
Juanma Díaz <juanma99dr@gmail.com>
2022-05-19 21:42:37 +0200

    Update settings.py

--- pokedexPokemon/settings.py
+++ pokedexPokemon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-unmt0ahwvbc5!!z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['desolate-hamlet-96146.herokuapp.com']
 
 
 # Application definition
